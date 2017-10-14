cdd48cdcb44e1f226bf97d715ff332b0a3d2c424
Aditya rao <adityasrao1000@gmail.com>
2017-10-14 22:31:47 +0530

    Duplicate words algorithm added

--- src/algorithms/string/AllSubStringPalindroms.java
+++ src/algorithms/string/AllSubStringPalindroms.java
@@ -1,13 +1,15 @@
 package algorithms.string;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AllSubStringPalindroms {
 	
-
+  
   public static void main(String args[]){
 
 	Scanner sc = new Scanner(System.in);
@@ -24,11 +26,11 @@ public class AllSubStringPalindroms {
 		System.out.println("string cannot contain white spaces. Please enter a string without white spaces.");
 		System.exit(0);
 	}
-	//create arraylist to hold all substrings
+	//create array list to hold all substrings
 	ArrayList<String> list = new ArrayList<String>();
-	//create arraylist to hold reversed substrings
+	//create array list to hold reversed substrings
 	ArrayList<String> rev = new ArrayList<String>();
-	
+	 
 	//algorithm to extract every possible substring
 	for(int i=0;i<s.length();i++)
 	{
@@ -42,11 +44,11 @@ public class AllSubStringPalindroms {
 	
 	//algorithm to  extract and reverse all substrings
 	 for(String obj:list)  {
+	   //create temporary string to hold substring values
 	   String temp = "";
 	   
 	    char ch1[] =  obj.toCharArray(); 
-	    for(int i=ch1.length-1;i>=0;i--) {
-	      
+	    for(int i=ch1.length-1;i>=0;i--) {	      
 	      temp += ch1[i];
 	    }
 	    rev.add(temp);
@@ -55,9 +57,18 @@ public class AllSubStringPalindroms {
 	
 	//only keep strings that are the same in both lists
 	list.retainAll(rev);
-	//print results
+	if(list.isEmpty()) {
+		System.out.println("no palindrome's exist");
+		System.exit(0);
+	}
+	
+	//covert reversed list into set so do not have same substrings repeated
+	Set<String> set = new HashSet<String>(list);
 	System.out.println("Total number of palindrome strings: "+ list.size());
-	System.out.println(list);
+	
+	//print result, for result that match's total number of string print list instead of set
+	System.out.println(set);
+	System.out.println("\nnote: similar substrings are not printed more than once");
 	}
 
 }
