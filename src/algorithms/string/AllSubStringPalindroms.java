ff9d5bf0c32ed6850ae0e4055f82b31bf8ba0200
Aditya rao <adityasrao1000@gmail.com>
2017-10-14 16:26:47 +0530

    all substring palindrome example

--- src/algorithms/string/AllSubStringPalindroms.java
+++ src/algorithms/string/AllSubStringPalindroms.java
@@ -7,19 +7,23 @@ public class AllSubStringPalindroms {
 
 	public static void main(String args[]){
 	String s ="jkbvtjtiop";
+	//create arraylist to hold all substrings
 	ArrayList<String> list = new ArrayList<String>();
+	//create arraylist to hold reversed substrings
 	ArrayList<String> rev = new ArrayList<String>();
+	
+	//algorithm to extract every possible substring
 	for(int i=0;i<s.length();i++)
 	{
-	for(int j=i+1;j<=s.length();j++){
-	if(s.substring(i,j).length()!= 1)
-	{
-	 list.add(s.substring(i,j));
+	  for(int j=i+1;j<=s.length();j++){
+	    if(s.substring(i,j).length()!= 1)
+		{
+		  list.add(s.substring(i,j));
+		}
+	  }
 	}
-
-	}
-
-	}
+	
+	//algorithm to reverse extract and reverse all substrings
 	 for(String obj:list)  {
 	   String temp = "";
 	   
@@ -31,7 +35,10 @@ public class AllSubStringPalindroms {
 	    rev.add(temp);
 
 	 }  
+	
+	//only keep strings that are the same in both lists
 	list.retainAll(rev);
+	//print results
 	System.out.println(list);
 	}
 
