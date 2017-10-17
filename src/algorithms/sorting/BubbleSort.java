d4e7baa4489b3a4c97b35a976b50f933c864e679
Aditya rao <adityasrao1000@gmail.com>
2017-10-17 16:31:04 +0530

    test modified

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -1,7 +1,7 @@
 package algorithms.sorting;
 
 public class BubbleSort {
-    public static int[] bubbleSort(int[] arr){
+    public static void bubbleSort(int[] arr){
 	  int n = arr.length;  
         int temp = 0;  
         for(int i=0; i < n; i++){  
@@ -14,17 +14,16 @@ public class BubbleSort {
                 }   
             }
         }  
-	    return arr;
     }
 	public static void main(String[] args) {
 		
 		int[] arr = {4,2,1,7,87,56,8,13};
-		int[] a = bubbleSort(arr);
+		bubbleSort(arr);
 		
 		//print sorted array
-		 for(int k=0; k < a.length; k++){  
-             System.out.print(a[k] +" ");  
-         }  
-         System.out.println(); 
+		 for(int k=0; k < arr.length; k++){  
+	            System.out.print(arr[k] +" ");  
+	        }  
+	        System.out.println();
 	}
 }
--- src/algorithms/sorting/test/BubbleSortTest.java
+++ src/algorithms/sorting/test/BubbleSortTest.java
@@ -1,17 +1,23 @@
 package algorithms.sorting.test;
 
 import static org.junit.Assert.*;  
-import algorithms.sorting.BubbleSort;  
+import algorithms.sorting.BubbleSort;    
+import org.junit.Before;   
 import org.junit.Test;  
 
 public class BubbleSortTest {
 	
+	BubbleSort sort = new BubbleSort();
+	int[] arr1 = {4,2,1,7,87,56,8,13};
+    int[] arr2 = {1,2,4,7,8,13,56,87};
+    
+    @Before  
+    public void setUp() throws Exception {     	
+    	BubbleSort.bubbleSort(arr1);  	
+    } 
+    
 	@Test
-    public void test() throws Exception {
-        int[] unsorted = {2,1,9,6,5,4};
-        int[] sorted = {1,2,4,5,6,9};
-        
-        assertArrayEquals(sorted,BubbleSort.bubbleSort(unsorted));
-
+    public void bubbleSort() throws Exception {		     
+        assertArrayEquals(arr1,arr2);
     }
 }
