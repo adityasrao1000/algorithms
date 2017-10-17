72ff664d0ba3e0b2451e88e8351d907e6e656db6
Aditya rao <adityasrao1000@gmail.com>
2017-10-17 10:31:48 +0530

    spelling mistake correction

--- src/algorithms/sorting/test/BubbleSortTest.java
+++ src/algorithms/sorting/test/BubbleSortTest.java
@@ -1,10 +1,11 @@
-package algoritms.sorting.test;
+package algorithms.sorting.test;
 
 import static org.junit.Assert.*;  
 import algorithms.sorting.BubbleSort;  
 import org.junit.Test;  
 
 public class BubbleSortTest {
+	
 	@Test
     public void test() throws Exception {
         int[] unsorted = {2,1,9,6,5,4};
