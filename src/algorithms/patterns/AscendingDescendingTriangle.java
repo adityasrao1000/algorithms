0e774022aa22166070713789e6512b3db58dc2bb
Aditya rao <adityasrao1000@gmail.com>
2017-12-15 20:54:39 +0530

    more algorithms added

---
+++ src/algorithms/patterns/AscendingDescendingTriangle.java
@@ -0,0 +1,42 @@
+package algorithms.patterns;
+
+import java.util.Scanner;
+
+public class AscendingDescendingTriangle {
+	
+	  static void printTriangle(int rows) {
+		int count=0;
+	    for(int i=0;i<rows;i++) {
+	    	
+	    	count++;
+	    	for(int j=0;j<count;j++) {
+	    		System.out.print("* ");
+	    	}
+	    	
+	    	System.out.println();    	
+	    }
+	    printTriangleDec(rows);
+	  }
+	  
+	  static void printTriangleDec(int rows) {
+			int count=rows;
+		    for(int i=0;i<rows;i++) {
+		    	
+		    	count--;
+		    	for(int j=0;j<count;j++) {
+		    		System.out.print("* ");
+		    	}
+		    	
+		    	System.out.println();    	
+		    }
+		  }
+	  public static void main(String[] args) {
+			
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the number of rows");
+	    int rows = sc.nextInt();
+	    sc.close();
+	    printTriangle(rows);
+	  }
+  
+}
