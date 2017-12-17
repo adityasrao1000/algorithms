eab3d4cf35a1843f1ae5e1120a4cda34a158dbda
Aditya rao <adityasrao1000@gmail.com>
2017-12-17 10:24:58 +0530

    new algorithms added

--- src/algorithms/list/Linked.java
+++ src/algorithms/list/Linked.java
@@ -1,54 +1,5 @@
 package algorithms.list;
 
-class Node<T>{	  
-	Node<T> next;
-	T value;
-	 
-    Node(T value){
-	  this.value = value;
-	}
-}
-
-class Llist<T>{
-	
-  
-  
-  Node<T> head;
-
-  Llist(){
-    this.head= null;
-  }
-  
-  void print(){
-	  
-   Node<T> n=head;
-   
-   while(n!=null){
-    System.out.println(n.value);
-    n=n.next;
-
-   }
-  }
-
-  Node<T> iterate(){
-   Node<T> n =this.head;
-   while(n.next!=null){  
-	   n=n.next;
-   }
-   return  n;
-  }
-  void add(T n){
-
-    if(this.head==null){
-     Node<T> node = new Node<T>(n);
-     head =node;
-     return;
-    }
-    Node<T> last = iterate();
-    Node<T> node = new Node<>(n);
-    last.next = node;
-  }
-}
 
 public class Linked {
 	
---
+++ src/algorithms/list/LinkedListOperations.java
@@ -0,0 +1,18 @@
+package algorithms.list;
+
+public class LinkedListOperations {
+	
+ public static void main(String[] args) {  
+   
+   System.out.println("Integer linked list");
+   Llist<Integer> l1 =new Llist<>();
+   l1.add(1);
+   l1.add(2);
+   l1.add(3);
+   l1.add(4);
+   l1.add(5);
+   l1.print();
+   
+ }  
+
+} 
