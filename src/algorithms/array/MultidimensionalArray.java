5bd42de47265798bc6517cb2a8319202f05f4520
Aditya Rao <adityasrao1000@gmail.com>
2018-01-29 14:24:01 +0530

    multidimensional array input

---
+++ src/algorithms/array/MultidimensionalArray.java
@@ -0,0 +1,25 @@
+package algorithms.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultidimensionalArray{
+	
+	public static void main(String args[]){
+		int[][][] a = new int[3][3][3];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0;i<3;i++){
+		  for(int j=0;j<3;j++){
+		    for(int k=0;k<3;k++){
+		     System.out.print("enter value for a["+i+"]"+"["+j+"]"+"["+k+"]");
+		     a[i][j][k]= sc.nextInt();
+		    }
+		  }
+		}
+		
+		sc.close();
+		//print values of the array
+		System.out.println(Arrays.deepToString(a));
+	}
+}
