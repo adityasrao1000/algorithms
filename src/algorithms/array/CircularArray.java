b8e89dbf1d02222d5d62448a3ce9728a2de0e1ce
Aditya Rao <adityasrao1000@gmail.com>
2018-06-09 11:08:33 +0530

    indentation

--- src/algorithms/array/CircularArray.java
+++ src/algorithms/array/CircularArray.java
@@ -2,50 +2,50 @@ package algorithms.array;
 
 import java.util.Scanner;
 
-public class CircularArray{
+public class CircularArray {
 
-	static void circular(char[] a,char ch){
-		int i=0;
-		int index =-1;
-		
-		//check if the element exists
-		for(i=0;i<a.length;i++){
-		  if(a[i]==ch){
-		    index= i;
-		  }
+	static void circular(char[] a, char ch) {
+		int i = 0;
+		int index = -1;
+
+		// check if the element exists
+		for (i = 0; i < a.length; i++) {
+			if (a[i] == ch) {
+				index = i;
+			}
 		}
-		
-		//print if element exists
-		if(index>-1){
-			 for(i=index;i<a.length;i++){
-			    System.out.print(a[i]);
-			  }
-			
-			  for(i=0;i<index;i++){
-			    System.out.print(a[i]);
-			  }
+
+		// print if element exists
+		if (index > -1) {
+			for (i = index; i < a.length; i++) {
+				System.out.print(a[i]);
+			}
+
+			for (i = 0; i < index; i++) {
+				System.out.print(a[i]);
+			}
 			return;
 		}
-		
+
 		System.out.println("The element does not exist");
 	}
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the a string greater than 0");
 		String inp = sc.nextLine();
-		
+
 		System.out.println("Enter a character ");
 		char[] a = inp.toCharArray();
 		String s = sc.next();
-		
-		while(s.length()!=1) {
+
+		while (s.length() != 1) {
 			System.out.println("Please enter only one character");
 			s = sc.next();
 		}
-		
+
 		char search = s.charAt(0);
-		sc.close();	
-		circular(a,search);
+		sc.close();
+		circular(a, search);
 	}
 }
--- src/algorithms/array/MatrixMultiplication.java
+++ src/algorithms/array/MatrixMultiplication.java
@@ -3,40 +3,40 @@ package algorithms.array;
 import java.util.Scanner;
 
 public class MatrixMultiplication {
- 
+
 	public static void main(String args[]) {
 		int[][] a1 = new int[2][3];
 		Scanner sc = new Scanner(System.in);
-		
-		for(int i=0;i<2;i++){
-		  for(int j=0;j<3;j++){
-		     System.out.print("enter value for a1["+i+"]"+"["+j+"]");
-		     a1[i][j]= sc.nextInt();
-		  }
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print("enter value for a1[" + i + "]" + "[" + j + "]");
+				a1[i][j] = sc.nextInt();
+			}
 		}
-		
+
 		int[][] a2 = new int[3][2];
-		
-		for(int i=0;i<3;i++){
-		  for(int j=0;j<2;j++){
-		     System.out.print("enter value for a2["+i+"]"+"["+j+"]");
-		     a2[i][j]= sc.nextInt();
-		  }
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.print("enter value for a2[" + i + "]" + "[" + j + "]");
+				a2[i][j] = sc.nextInt();
+			}
 		}
-		if(sc!=null) {
-		  sc.close();
+		if (sc != null) {
+			sc.close();
 		}
-		
-		int sum=0;
-		for(int i=0;i<2;i++){
-		  for(int j=0;j<2;j++){
-		    for(int k=0;k<3;k++) {
-	          sum += a1[i][k]*a2[k][j];
-		    }
-		  }
+
+		int sum = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 3; k++) {
+					sum += a1[i][k] * a2[k][j];
+				}
+			}
 		}
-		
+
 		System.out.println(sum);
-		
+
 	}
 }
